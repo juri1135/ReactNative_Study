a4e5c926029833cbade0ff5988c4a6849e9d47d5
juri1135 <juri113518@gmail.com>
2024-04-09 15:19:13 +0900

    Building a framework based on an iOS-based weather app (configuring current location and temperature container and expected temperature container for each week)

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.normalContainer}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+        <View style={styles.weatherToday}>
+          <Text style={styles.tempToday}>27</Text>
+          <Text style={styles.descToday}>Sunny</Text>
+        </View>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.tempWeek}>19</Text>
+          <Text style={styles.descWeek}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempWeek}>10</Text>
+          <Text style={styles.descWeek}>Cloudy</Text>
+        </View>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  normalContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(60, 113, 168, 0.8)',
+  },
+  //장소
+  city: {
+    flex: 1.3,
+    marginTop: 90,
     alignItems: 'center',
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 40,
+  },
+  //오늘의 기온 및 설명
+  weatherToday: {
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tempToday: {
+    color: 'white',
+    fontSize: 75,
+  },
+  descToday: {
+    color: 'white',
+    fontSize: 25,
+  },
+  //주차별 날씨
+  weather: {
+    flex: 3,
+    backgroundColor: 'rgba(39, 111, 187, 0.8)',
+    margin: 20,
+    borderRadius: 10,
+    flexDirection: 'row',
+  },
+  day: {
+    margin: 20,
+    flexDirection: 'col',
+    alignItems: 'center',
+  },
+  tempWeek: {
+    color: 'white',
+    fontSize: 10,
+  },
+  descWeek: {
+    color: 'white',
+    fontSize: 10,
   },
 });
